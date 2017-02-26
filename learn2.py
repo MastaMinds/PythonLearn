128ed6112fd56668ff183416bef9ce8c3c29bb26
MastaMinds <ah_mm_mad@yahoo.com>
2017-02-27 00:21:49 +0300

    First commit

---
+++ learn2.py
@@ -0,0 +1,27 @@
+# Try and except commands
+Userip=raw_input("Enter a number: ")
+try:
+    ival=int(Userip)
+except:
+    ival=-1
+
+if ival>0:
+    print("Well done!")
+else:
+    print("Not a number..")
+    
+    
+
+str1="Hello Ahmed"
+try:
+    a=int(str1)
+except:
+    a=-1
+print "First",a
+
+str2="123"
+try:
+    b=int(str2)
+except:
+    b=-1
+print "Second",b
