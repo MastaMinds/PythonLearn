066050c8d3665915b432797bd2203faf3338b19b
MastaMinds <a.mahgoub@unsw.edu.au>
2021-08-15 18:54:20 +1000

    POD code cylinder done

--- POD_Cylinder/POD_2D_Cylinder_Ahmhmd.py
+++ POD_Cylinder/POD_2D_Cylinder_Ahmhmd.py
@@ -7,9 +7,11 @@ Made by Ahmhmd UNSW - 2021
 """
 
 import numpy as np
+import matplotlib as mp
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as mplot3d
 import pandas as pd
+from scipy.interpolate import griddata
 
 Main_path = 'C:/Users/ahmhm/POD_Cylinder/Cylinder_2D_Laminar/'
 
@@ -35,8 +37,107 @@ for i in range(N):
     X[:,i] = data_all[i]['U:0']
     # X[:,i] = data_all[i]['vorticity:2']
     print('Finished data file number'+str(i+1))
-    
 
-x = test_table['Points:0']
-y = test_table['Points:1']
+X_mean = X.mean(axis=1)
+for k in range(N):
+    X[:,k] = X[:,k] - X_mean
+    
+U, Sigma, V = np.linalg.svd(X)
+
+y = test_table['Points:0']
+x = test_table['Points:1']
 z = test_table['vorticity:2']
+
+y_a, x_a = np.mgrid[-0.2:1:0.0005, -0.5:0.5:0.0005]
+z_a = griddata((x,y), z, (x_a, y_a), method='linear')
+U0 = griddata((x,y), U[:,0], (x_a, y_a), method='linear')
+U1 = griddata((x,y), U[:,1], (x_a, y_a), method='linear')
+U2 = griddata((x,y), U[:,2], (x_a, y_a), method='linear')
+U3 = griddata((x,y), U[:,3], (x_a, y_a), method='linear')
+U4 = griddata((x,y), U[:,4], (x_a, y_a), method='linear')
+
+x_a[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+y_a[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+z_a[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+U0[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+U1[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+U2[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+U3[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+U4[np.sqrt(x_a**2 + y_a**2) <= 0.1] = np.nan
+
+
+ax00 = plt.axes(projection='3d')
+ax00.view_init(-90, 0)
+ax00.plot_surface(x_a, y_a, z_a, cmap= 'Spectral',linewidth=0, 
+                  vmin = -2, vmax = 2,antialiased=False)
+ax00.set_ylim(-0.1, 1)
+ax00.set_xlim(-0.5, 0.5)
+
+ax01 = plt.axes(projection='3d')
+ax01.view_init(-90, 0)
+ax01.plot_surface(x_a, y_a, U2, cmap= 'Spectral',linewidth=0,
+                  antialiased=False)
+ax01.set_ylim(-0.1, 1)
+ax01.set_xlim(-0.5, 0.5)
+
+TriGrid = mp.tri.Triangulation(x, y)
+
+fig = plt.figure()
+ax = mplot3d.Axes3D(fig)
+ax.view_init(-90, 0)
+surf = ax.plot_trisurf(x, y, z, triangles = TriGrid.triangles, vmin = -2,
+                       vmax = 2, cmap = 'Spectral', edgecolor = 'none',
+                       linewidth = 0, antialiased = False) # vmax and vmin: 'None'
+ax.set_ylim(-0.5, 0.5)
+ax.set_xlim(-0.5, 0.5)
+plt.colorbar(surf)
+
+fig0 = plt.figure()
+ax0 = mplot3d.Axes3D(fig0)
+ax0.view_init(-90, 0)
+surf0 = ax0.plot_trisurf(x, y, U[:,0], triangles = TriGrid.triangles, vmin = None,
+                       vmax = None, cmap = 'Spectral', edgecolor = 'none',
+                       linewidth = 0, antialiased = False) # vmax and vmin: 'None'
+ax0.set_ylim(0, 1)
+ax0.set_xlim(-0.5, 0.5)
+plt.colorbar(surf0)
+
+fig1 = plt.figure()
+ax1 = mplot3d.Axes3D(fig1)
+ax1.view_init(-90, 0)
+surf1 = ax1.plot_trisurf(x, y, U[:,1], triangles = TriGrid.triangles, vmin = None,
+                       vmax = None, cmap = 'Spectral', edgecolor = 'none',
+                       linewidth = 0, antialiased = False) # vmax and vmin: 'None'
+ax1.set_ylim(0, 1)
+ax1.set_xlim(-0.5, 0.5)
+plt.colorbar(surf1)
+
+fig2 = plt.figure()
+ax2 = mplot3d.Axes3D(fig2)
+ax2.view_init(-90, 0)
+surf2 = ax2.plot_trisurf(x, y, U[:,2], triangles = TriGrid.triangles, vmin = None,
+                       vmax = None, cmap = 'Spectral', edgecolor = 'none',
+                       linewidth = 0, antialiased = False) # vmax and vmin: 'None'
+ax2.set_ylim(0, 1)
+ax2.set_xlim(-0.5, 0.5)
+plt.colorbar(surf2)
+
+fig3 = plt.figure()
+ax3 = mplot3d.Axes3D(fig3)
+ax3.view_init(-90, 0)
+surf3 = ax3.plot_trisurf(x, y, U[:,3], triangles = TriGrid.triangles, vmin = None,
+                       vmax = None, cmap = 'Spectral', edgecolor = 'none',
+                       linewidth = 0, antialiased = False) # vmax and vmin: 'None'
+ax3.set_ylim(0, 1)
+ax3.set_xlim(-0.5, 0.5)
+plt.colorbar(surf3)
+
+fig4 = plt.figure()
+ax4 = mplot3d.Axes3D(fig4)
+ax4.view_init(-90, 0)
+surf4 = ax4.plot_trisurf(x, y, U[:,4], triangles = TriGrid.triangles, vmin = None,
+                       vmax = None, cmap = 'Spectral', edgecolor = 'none',
+                       linewidth = 0, antialiased = False) # vmax and vmin: 'None'
+ax4.set_ylim(0, 1)
+ax4.set_xlim(-0.5, 0.5)
+plt.colorbar(surf4)
---
+++ POD_Cylinder/griddata_try.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Test interpolation of grid data
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+
+def func(x, y):
+    return x*(1-x)*np.cos(4*np.pi*x) * np.sin(4*np.pi*y**2)**2
+
+grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
+
+rng = np.random.default_rng()
+points = rng.random((1000, 2))
+values = func(points[:,0], points[:,1])
+
+xa = np.linspace(0, 1, 100)
+ya = np.linspace(0, 1, 100)
+
+grid_z0 = griddata(points, values, (grid_x, grid_y), method='nearest')
+grid_z1 = griddata(points, values, (grid_x, grid_y), method='linear')
+grid_z2 = griddata(points, values, (grid_x, grid_y), method='cubic')
+grid_za = griddata(points, values, (xa, ya), method='nearest')
+
+plt.subplot(221)
+plt.imshow(func(grid_x, grid_y).T, extent=(0,1,0,1), origin='lower')
+plt.plot(points[:,0], points[:,1], 'k.', ms=1)
+plt.title('Original')
+plt.subplot(222)
+plt.imshow(grid_z0.T, extent=(0,1,0,1), origin='lower')
+plt.title('Nearest')
+plt.subplot(223)
+plt.imshow(grid_z1.T, extent=(0,1,0,1), origin='lower')
+plt.title('Linear')
+plt.subplot(224)
+plt.imshow(grid_z2.T, extent=(0,1,0,1), origin='lower')
+plt.title('Cubic')
+plt.gcf().set_size_inches(6, 6)
